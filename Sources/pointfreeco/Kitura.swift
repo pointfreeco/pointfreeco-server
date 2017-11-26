32d61f87a7486a478c54c4349e7a618f7d272e27
Stephen Celis <stephen@stephencelis.com>
2017-11-26 13:17:41 -0500

    Cleanup

--- Sources/pointfreeco/Kitura.swift
+++ Sources/pointfreeco/Kitura.swift
@@ -8,13 +8,12 @@ import Prelude
 func updateResponse(_ kituraResponse: RouterResponse) -> (HttpPipeline.Response) -> Void {
 
   return { res in
+    kituraResponse.status(HTTPStatusCode(rawValue: res.status.rawValue)!)
     var headers = kituraResponse.headers
     res.headers.map(get(\.pair)).forEach { key, value in headers.append(key, value: value) }
     kituraResponse.headers = headers
-
-    kituraResponse.status(HTTPStatusCode(rawValue: res.status.rawValue)!)
-
     kituraResponse.send(data: res.body)
+
     try? kituraResponse.end()
   }
 }
