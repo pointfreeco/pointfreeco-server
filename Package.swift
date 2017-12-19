92480288de15cfba98f57d279543ff5cdcdebc86
Stephen Celis <stephen@stephencelis.com>
2017-12-19 00:24:57 -0500

    Bump PointFree

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.0.0"),
-    .package(url: "https://github.com/pointfreeco/pointfreeco.git", .revision("bf82177")),
+    .package(url: "https://github.com/pointfreeco/pointfreeco.git", .revision("36c416e")),
   ],
   targets: [
     .target(
