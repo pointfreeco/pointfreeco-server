ce2866ad19af849aad8ff2f3ab4fd8b18d605714
Stephen Celis <stephen@stephencelis.com>
2018-01-29 19:02:32 -0500

    Bump

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.1.0"),
     .package(url: "https://github.com/IBM-Swift/Kitura-Compression", from: "2.1.0"),
-    .package(url: "https://github.com/pointfreeco/pointfreeco.git", .revision("c42e0a5")),
+    .package(url: "https://github.com/pointfreeco/pointfreeco.git", .revision("75e3130")),
   ],
   targets: [
     .target(
